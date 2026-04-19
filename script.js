5aff048dcd24f5739ce7a2ea45fb28a722b8815f
Ondor-R <rehanondor@gmail.com>
2026-04-20 00:59:26 +0700

    fix: update asset paths for consistency across HTML, CSS, and JS files

--- script.js
+++ script.js
@@ -55,11 +55,11 @@ socialIcons.forEach((icon) => {
 
 //--------------------------------------------------------------BOOKIE IMG AT FOOTER//
 bookieImg.addEventListener('mouseover', () => {
-    bookieImg.src = 'assets/pngs/bookie-1.png';
+    bookieImg.src = './assets/pngs/bookie-1.png';
 })
 
 bookieImg.addEventListener('mouseout', () => {
-    bookieImg.src = 'assets/pngs/bookie-0.png';
+    bookieImg.src = './assets/pngs/bookie-0.png';
 })
 
 bookieImg.addEventListener('click', () => {
